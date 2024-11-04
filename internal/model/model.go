4d4b37f1530dc9a84bc64611dc9e91b77302ce71
Jonathan Langley <jono.langley@azarc.io>
2024-11-04 10:08:18 +0100

    Added marshalling from JSON to EDI

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,29 +1,41 @@
 package model
 
 type EdiType string
+type JsonSchemaType string
 
 const (
 	EdiTypeSegment   EdiType = "segment"
 	EdiTypeElement   EdiType = "element"
 	EdiTypeComponent EdiType = "component"
+
+	JsonSchemaTypeArray   JsonSchemaType = "array"
+	JsonSchemaTypeObject  JsonSchemaType = "object"
+	JsonSchemaTypeNumber  JsonSchemaType = "number"
+	JsonSchemaTypeString  JsonSchemaType = "string"
+	JsonSchemaTypeBoolean JsonSchemaType = "boolean"
+	JsonSchemaTypeInteger JsonSchemaType = "integer"
+	JsonSchemaTypeUnknown JsonSchemaType = ""
 )
 
 type (
 	Schema struct {
 		Schema string `json:"$schema"`
+		Self   *struct {
+			Schema string `json:"$schema"`
+		} `json:"self"`
 		*Property
 	}
 
 	Property struct {
-		Description string     `json:"description"`
-		Type        string     `json:"type"`
-		XEdi        XEdi       `json:"x-edi"`
-		Properties  Properties `json:"properties"`
-		Items       *Property  `json:"items"`
-		IsRequired  bool       `json:"isRequired"`
-		Const       any        `json:"const"`
-		MinItems    int        `json:"minItems"`
-		MaxItems    int        `json:"maxItems"`
+		Description string         `json:"description"`
+		Type        JsonSchemaType `json:"type"`
+		XEdi        XEdi           `json:"x-edi"`
+		Properties  Properties     `json:"properties"`
+		Items       *Property      `json:"items"`
+		IsRequired  bool           `json:"isRequired"`
+		Const       any            `json:"const"`
+		MinItems    int            `json:"minItems"`
+		MaxItems    int            `json:"maxItems"`
 	}
 
 	Properties map[string]*Property
